46c6b521e99de683e8f56601a70c0217d2f56bcb
Jacob Hanna <21013107@students.latrobe.edu.au>
2023-08-18 15:24:54 +1000

    Lab 2 Stuff... again lol

--- server.js
+++ server.js
@@ -2,18 +2,20 @@ const express = require('express');
 const child_process = require('child_process');
 // Create a new Express web application
 const app = express();
+// Grab date to be used
+const d = Date();
 // Set handler for the index of the website
 app.get('/', (req, res) => {
 // Run the system `fortune` command and respond with the message
 child_process.exec('fortune', (error, message) => {
 if(error === null) {
-res.send(message);
+res.send(d + " " + "Your Fortune is: " + message);
 } else {
-res.send('Error: ' + error);
+res.send(d + " " + 'Error: ' + error);
 }
 })
 });
 // Start web application server
 app.listen(3000, () => {
-console.log('Server started');
+console.log('Lab 2 Server is Online! :D');
 });
